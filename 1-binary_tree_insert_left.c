a9af9a48a258dd08a8211ee978df6f9d75835e33
abed-naanaa <abednaanaa12@gmail.com>
2025-02-05 18:42:03 +0000

    adj 1 and 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent->right != NULL)
 	{
-		new_node->left = parent->left;
+		
 		parent->left->parent = new_node;
 	}
 
