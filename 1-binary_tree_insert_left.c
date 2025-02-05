3f9e3d612c818e64167119c96157b5a137356d12
abed-naanaa <abednaanaa12@gmail.com>
2025-02-05 18:30:27 +0000

    adj 1 and 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,24 +10,27 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-binary_tree_t *new_node;
+    binary_tree_t *new_node;
 
-if (parent == NULL)
-return (NULL);
+    if (parent == NULL)
+        return (NULL);
 
-new_node = malloc(sizeof(binary_tree_t));
-if (new_node == NULL)
-return (NULL);
+    new_node = malloc(sizeof(binary_tree_t));
+    if (new_node == NULL)
+        return (NULL);
 
-new_node->n = value;
-new_node->left = NULL;
-new_node->right = NULL;
+    new_node->n = value;
+    new_node->left = NULL;
+    new_node->right = NULL;
 
-if (parent->left != NULL)
-{
-new_node->left = parent->left;
-}
-parent->left = new_node;
+    /* If parent already has a left child, the new node takes its place */
+    if (parent->left != NULL)
+    {
+        /* The old left child will be moved to the new node's left */
+        new_node->left = parent->left;
+    }
 
-return (new_node);
+    parent->left = new_node; /* Set the new node as the left child */
+
+    return (new_node);
 }
